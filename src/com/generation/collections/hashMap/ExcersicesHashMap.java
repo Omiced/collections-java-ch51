6f33fd5bfb4299b28e2b6b7818a8626cd6d696ba
Jaqui <jaquelinejesus.leon@gmail.com>
2025-03-13 13:25:17 -0600

    Agregue linea con mi nombre

--- src/com/generation/collections/hashMap/ExcersicesHashMap.java
+++ src/com/generation/collections/hashMap/ExcersicesHashMap.java
@@ -4,15 +4,17 @@ import java.util.HashMap;
 
 public class ExcersicesHashMap {
     public static void main(String[] args) {
-        String[] words = {"java", "python", "golang", "kotlin", "python", "java", "rust", "typescript", "rust", "kotlin","rust"};
+        String[] words = {"java", "python", "golang", "kotlin", "python", "java", "rust", "typescript", "rust", "kotlin", "rust"};
         HashMap<String, Integer> wordCount = new HashMap<>();
-        for(String word: words){
-            wordCount.put(word, wordCount.getOrDefault(word,0) + 1);
+        for (String word : words) {
+            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
         }
 
         System.out.println(wordCount);
-
+        HashMap<String, Integer> learners = new HashMap<>();
+        learners.put("Jaqueline", 29);
     }
-
-    HashMap<String,Integer>learners = new HashMap<>();
 }
+
+
+
