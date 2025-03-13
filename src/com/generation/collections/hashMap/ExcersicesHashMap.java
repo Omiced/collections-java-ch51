c6d765fdf48c91034b135e714bf9e19e7919f356
DavidMesVal <davidmesvaldi@gmail.com>
2025-03-13 13:09:04 -0600

    actualizacion

--- src/com/generation/collections/hashMap/ExcersicesHashMap.java
+++ src/com/generation/collections/hashMap/ExcersicesHashMap.java
@@ -4,18 +4,17 @@ import java.util.HashMap;
 
 public class ExcersicesHashMap {
     public static void main(String[] args) {
-        String[] words = {"java", "python", "golang", "kotlin", "python", "java", "rust", "typescript", "rust", "kotlin","rust"};
+        String[] words = {"java", "python", "golang", "kotlin", "python", "java", "rust", "typescript", "rust", "kotlin", "rust"};
         HashMap<String, Integer> wordCount = new HashMap<>();
-        for(String word: words){
-            wordCount.put(word, wordCount.getOrDefault(word,0) + 1);
+        for (String word : words) {
+            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
         }
 
         System.out.println(wordCount);
-    HashMap<String,Integer>learners = new HashMap<>();
-	learners.put("Adair",24);
-    learners.put("Osbaldo",30);
-    learners.put("Josue", 31);
 
     }
 
+    HashMap<String, Integer> learners = new HashMap<>();
+         learners.put("Adair",24);
+         learners.put("David",29);
 }
